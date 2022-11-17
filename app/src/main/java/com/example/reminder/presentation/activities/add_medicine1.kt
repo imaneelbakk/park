f1a67cf758baf6ff11661acf4d1803f7dbbf7d4b
imane <elbakkouriimane3@gmail.com>
2022-11-17 02:57:56 +0100

    addMed_Navbar

---
+++ app/src/main/java/com/example/reminder/presentation/activities/add_medicine1.kt
@@ -0,0 +1,11 @@
+package com.example.reminder.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_medicine1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_medicine1)
+    }
+}
